9d3e0d71ed35940d8889656b104a8f0cc5218ad2
JoseFontana <josealbertonicolasfontanaferrino@upc.edu.ar>
2025-10-20 19:56:46 -0300

    Lan 1era Version
    Lan 1era Version y cambio a Rider
    Seguire probando para ver si hay otro error

---
+++ Source/LosMaresDeNeptuno/Public/Lan_Coneccion.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../../../../../UE_5.6/Engine/Source/Developer/CQTest/Public/ObjectBuilder.h"
+#include "../../../../../UE_5.6/Engine/Source/Runtime/CoreUObject/Public/UObject/ObjectPtr.h"
+
+#include "Lan_Coneccion.generated.h"
+
+class UButton; 
+{
+public:
+	
+};
+/**
+ * 
+ */
+UCLASS()
+class LOSMARESDENEPTUNO_API ULan_Coneccion : public UUserWidget
+{
+	GENERATED_BODY()
+
+
+public:
+
+private:
+
+	TObjectPtr<UButton>	Button;
+	
+};
